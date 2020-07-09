682584efb5e368c1f5ed329a4002cf01844459a1
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-07-09 16:35:51 +0430

    use nativebase UI

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -10,7 +12,11 @@ import store from "./redux";
 
 export default function App(): JSX.Element | null {
   const isLoadingComplete = useCachedResources();
-
+  Font.useFonts({
+    Roboto: require("native-base/Fonts/Roboto.ttf"),
+    Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+    ...Ionicons.font,
+  });
   if (!isLoadingComplete) {
     return null;
   } else {
