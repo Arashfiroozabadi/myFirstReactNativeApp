d0509d6d6399ec02b45aadbf9aeb35ec54ad638e
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-07-12 13:38:34 +0430

    add new color pattern

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,7 +4,9 @@ const tintColorDark = "#fff";
 export default {
   dark: {
     text: "#fff",
+    icons: "#fff",
     tint: tintColorDark,
+    placeholder: "grey",
     button: "#f4511e",
     headerBGC: "#f4511e",
     background: "#000",
@@ -13,7 +15,9 @@ export default {
   },
   light: {
     text: "#000",
+    icons: "black",
     tint: tintColorLight,
+    placeholder: "grey",
     button: "#61dafb",
     headerBGC: "#61dafb",
     background: "#fff",
