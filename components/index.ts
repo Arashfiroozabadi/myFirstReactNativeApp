4176cb3613802c90a43e9fb00481ee99ff5af4da
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-07-22 13:27:24 +0430

    new exports

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 export { default as Navigator } from "./Navigator";
 export { BaseButton } from "./Themed";
 export { IconBase } from "./Themed";
+export { View } from "./Themed";
+export { Text } from "./Themed";
